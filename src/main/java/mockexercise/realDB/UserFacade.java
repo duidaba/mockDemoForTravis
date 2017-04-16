bda01bd96bf575ca6314e8156cbb3b39f4030369
LarsM <lam@cphbusiness.dk>
2017-04-16 12:59:43 +0200

    skfks

--- src/main/java/mockexercise/realDB/UserFacade.java
+++ src/main/java/mockexercise/realDB/UserFacade.java
@@ -1,5 +1,7 @@
 package mockexercise.realDB;
 
+import java.util.HashMap;
+import java.util.Map;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
