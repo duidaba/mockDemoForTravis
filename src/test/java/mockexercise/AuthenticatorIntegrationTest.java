6bd29db2f01221dd1802d63aa97cf91dfdd9ed6c
LarsM <lam@cphbusiness.dk>
2017-04-16 11:14:52 +0200

    Doc

--- src/test/java/mockexercise/AuthenticatorIntegrationTest.java
+++ src/test/java/mockexercise/AuthenticatorIntegrationTest.java
@@ -3,6 +3,10 @@ package mockexercise;
 import mockexercise.realDB.UserFacade;
 import static org.mockito.Mockito.spy;
 
+/**
+ * Test with either a local database, or MySQL on travis-CI if running here
+ * @author plaul1
+ */
 public class AuthenticatorIntegrationTest extends AuthenticaterTest {
 
   private String getPuName(){
